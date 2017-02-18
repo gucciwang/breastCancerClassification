154441d4fb79f2787ba73be590e28194a0e3b790
Tate Allen <tatea7@yahoo.com>
2017-02-18 11:35:35 -0800

    Finished NN model

--- learning/data_batcher.py
+++ learning/data_batcher.py
@@ -2,22 +2,21 @@ import numpy as np
 import random
 
 class DataBatcher:
-    def __init__(self, data_file_path):
-        data = np.genfromtxt(data_file_path, delimiter=",")
-        data = np.delete(data, 0, 1)
-
-        labels = data[:,-1]
-        labels = (labels == 4).astype(float)
-        data = np.delete(data, -1, 1)
+    def __init__(self, train_x_path, train_y_path, test_x_path, test_y_path):
+        train_x = np.load(train_x_path)
+        train_y = np.load(train_y_path)
 
         self.samples = list()
-        for i in range(len(data) - int(0.1 * len(data))):
-            self.samples.append((data[i], labels[i]))
+        for i in range(len(train_x)):
+            self.samples.append((train_x[i], train_y[i]))
         self.epoch_samples = list(self.samples)
 
+        test_x = np.load(test_x_path)
+        test_y = np.load(test_y_path)
+
         self.test_samples = list()
-        for i in range(len(data) - int(0.1 * len(data)), len(data)):
-            self.test_samples.append((data[i], labels[i]))
+        for i in range(len(test_x)):
+            self.test_samples.append((test_x[i], test_y[i]))
 
     def reset_epoch(self):
         self.epoch_samples = list(self.samples)
--- learning/model.py
+++ learning/model.py
@@ -1,19 +1,26 @@
 import tensorflow as tf
 
 class Model:
-    def __init__(self, hidden_layer_size):
-        self.inputs = tf.placeholder(tf.float32, shape=[None, 9])
+    def __init__(self, hidden_layer_sizes):
+        self.inputs = tf.placeholder(tf.float32, shape=[None, 30])
         self.labels = tf.placeholder(tf.float32, shape=[None, 2])
 
-        W1 = tf.Variable(tf.truncated_normal([9, hidden_layer_size], stddev=0.1))
-        b1 = tf.Variable(tf.constant(0.1, shape=[hidden_layer_size]))
-        hidden_layer = tf.nn.tanh(tf.nn.xw_plus_b(self.inputs, W1, b1))
+        rows = 30
+        last_op = self.inputs
+        for i in hidden_layer_sizes:
+            W = tf.Variable(tf.truncated_normal([rows, i], stddev=0.1))
+            b = tf.Variable(tf.constant(0.1, shape=[i]))
+            hidden_layer = tf.nn.elu(tf.nn.xw_plus_b(last_op, W, b))
+            rows = i
+            last_op = hidden_layer
 
-        Wout = tf.Variable(tf.truncated_normal([hidden_layer_size, 2], stddev=0.1))
+        Wout = tf.Variable(tf.truncated_normal([rows, 2], stddev=0.1))
         bout = tf.Variable(tf.constant(0.1, shape=[2]))
-        output_layer = tf.nn.xw_plus_b(hidden_layer, Wout, bout)
+        output_layer = tf.nn.xw_plus_b(last_op, Wout, bout)
 
         self.probabilities = tf.nn.softmax(output_layer)
+
+        self.accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(self.probabilities, axis=1), tf.argmax(self.labels, axis=1)), tf.float32))
 
         self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=output_layer, labels=self.labels))
         self.train = tf.train.AdamOptimizer(1e-4).minimize(self.loss)
@@ -26,3 +33,6 @@ class Model:
 
     def get_loss(self, session, inputs, labels):
         return session.run(self.loss, feed_dict={self.inputs: inputs, self.labels: labels})
+
+    def get_accuracy(self, session, inputs, labels):
+        return session.run(self.accuracy, feed_dict={self.inputs: inputs, self.labels: labels})
