6224d459837643e6a3f774cfbc323972f4c63c3f
Tate Allen <tatea7@yahoo.com>
2017-02-18 17:18:29 -0800

    Cleaned up algos

--- learning/main.py
+++ learning/main.py
@@ -1,20 +1,23 @@
 from model import Model
 from log_reg_model import LogisticRegressionModel
+from soft_reg_model import SoftmaxRegressionModel
 from data_batcher import DataBatcher
 import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow as tf
 
-epochs = 500
+epochs = 400
 batch_size = 20
 
 batcher = DataBatcher("../normalizedData/trainingSetX.npy", "../normalizedData/trainingSetY.npy", "../normalizedData/testSetX.npy", "../normalizedData/testSetY.npy")
-model = Model([20])
+model = Model([40])
 log_model = LogisticRegressionModel()
+soft_model = SoftmaxRegressionModel()
 
 plot_x = list()
 plot_y = list()
 plot_y_log = list()
+plot_y_soft = list()
 with tf.Session() as session:
     session.run(tf.global_variables_initializer())
     epoch_index = 0
@@ -22,18 +25,25 @@ with tf.Session() as session:
         samples, labels = batcher.get_batch(batch_size)
         model.train_model(session, samples, labels)
         log_model.train_model(session, samples, labels)
+        soft_model.train_model(session, samples, labels)
         if batcher.epoch_finished():
             batcher.reset_epoch()
             test_samples, test_labels = batcher.get_test_batch()
-            # loss = model.get_loss(session, test_samples, test_labels)
             acc = model.get_accuracy(session, test_samples, test_labels)
             acc_log = log_model.get_accuracy(session, test_samples, test_labels)
+            acc_soft = soft_model.get_accuracy(session, test_samples, test_labels)
             plot_x.append(epoch_index+1)
             plot_y.append(acc)
             plot_y_log.append(acc_log)
-            print("Epoch {} -> {} <-> {}".format(epoch_index, acc, acc_log))
+            plot_y_soft.append(acc_soft)
+            print("Epoch {} ~ NN: {} | SoftR: {} | LogR: {}".format(epoch_index, acc, acc_soft, acc_log))
             epoch_index += 1
 
 sns.set_style("darkgrid")
-plt.plot(plot_x, plot_y, plot_x, plot_y_log)
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy on Test Set")
+plt.plot(plot_x, plot_y, label="Neural Net")
+plt.plot(plot_x, plot_y_log, label="Logistic Regression")
+plt.plot(plot_x, plot_y_soft, label="Softmax Regression")
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
 plt.show()
--- learning/soft_reg_model.py
+++ learning/soft_reg_model.py
@@ -1,23 +1,14 @@
 import tensorflow as tf
 
-class Model:
-    def __init__(self, hidden_layer_sizes):
-        with tf.variable_scope("neural_net_model"):
+class SoftmaxRegressionModel:
+    def __init__(self):
+        with tf.variable_scope("softmax_regression_model"):
             self.inputs = tf.placeholder(tf.float32, shape=[None, 30])
             self.labels = tf.placeholder(tf.float32, shape=[None, 2])
 
-            rows = 30
-            last_op = self.inputs
-            for i in hidden_layer_sizes:
-                W = tf.Variable(tf.truncated_normal([rows, i], stddev=0.1))
-                b = tf.Variable(tf.constant(0.1, shape=[i]))
-                hidden_layer = tf.nn.elu(tf.nn.xw_plus_b(last_op, W, b))
-                rows = i
-                last_op = hidden_layer
-
-            Wout = tf.Variable(tf.truncated_normal([rows, 2], stddev=0.1))
+            Wout = tf.Variable(tf.truncated_normal([30, 2], stddev=0.1))
             bout = tf.Variable(tf.constant(0.1, shape=[2]))
-            output_layer = tf.nn.xw_plus_b(last_op, Wout, bout)
+            output_layer = tf.nn.xw_plus_b(self.inputs, Wout, bout)
 
             self.probabilities = tf.nn.softmax(output_layer)
 
